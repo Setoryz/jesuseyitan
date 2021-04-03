9cb0786ac12eb4e68af89681035ba0e5dc0eea78
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-03 17:48:20 +0100

    portfolio: header component created

---
+++ portfolio/components/layout/Header/Header.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a className={styles.logo}>
+          <span className={styles.tag}> &lt;</span>
+          <span className={styles.logo__name}>Odukoya Jesuseyitan</span>
+          <span className={styles.tag}>/&gt;</span>
+        </a>
+      </Link>
+
+      <input className={styles.menu__btn} type="checkbox" id="menu-btn" />
+      <label className={styles.menu__icon} htmlFor="menu-btn">
+        <span className={styles.nav__icon}></span>
+      </label>
+
+      <nav className={styles.nav}>
+        <ul className={styles.menu}>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Skills</a>
+          </li>
+          <li>
+            <a>Experience</a>
+          </li>
+          <li>
+            <a>Contact Me</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
