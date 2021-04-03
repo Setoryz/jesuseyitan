aca4cad6216f6aad2814bc4a66ed8648abd83f5a
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-03 11:05:13 +0100

    portfolio: template code cleaned up

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Jesuseyitan</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
