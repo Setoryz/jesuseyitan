5a69f92131bcb60f363fd4546f6d1b52b5ab6f62
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-04 17:44:16 +0100

    PORTFOLIO: greeting component and scss module created

---
+++ portfolio/src/components/Greeting/Greeting.tsx
@@ -0,0 +1,32 @@
+import emoji from "react-easy-emoji";
+import styles from "./Greeting.module.scss";
+import welcomePerson from "../../assets/lottie/welcomePerson.lottie.json";
+
+const Greeting = () => {
+  return (
+    <>
+      <div className={styles.greeting}>
+        <div className="greet__main">
+          <div className="greeting__main">
+            <div className="greeting__text_container">
+              <h1 className="greeting__text">
+                Hello, My name is <br /> Jesuseyitan{" "}
+                <span className="wave-emoji">{emoji("👋")}</span>
+              </h1>
+              <p className={styles.greeting__text_p}>
+                I am a passionate Full Stack Software Developer 🚀 having an
+                experience of building Web pplications with Reactjs / Next JS /
+                Nodejs / ASP.NET Core and some other cool libraries and
+                frameworks.
+              </p>
+            </div>
+
+            <div className="greeting__image_container"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Greeting;
