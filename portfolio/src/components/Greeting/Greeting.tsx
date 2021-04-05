aa1e2a0d752f679bddc083eea2b3dcc8d5b0212f
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-05 09:07:22 +0100

    PORTFOLIO; Styling added to Greeting Section and Content modified

--- portfolio/src/components/Greeting/Greeting.tsx
+++ portfolio/src/components/Greeting/Greeting.tsx
@@ -7,22 +7,21 @@ const Greeting = () => {
   return (
     <>
       <div className={styles.greeting}>
-        <div className="greet__main">
-          <div className="greeting__main">
-            <div className="greeting__text_container">
-              <h1 className="greeting__text">
-                Hello, My name is <br /> Jesuseyitan{" "}
-                <span className="wave-emoji">{emoji("👋")}</span>
+        <div className={styles.greet__main}>
+          <div className={styles.greeting__main}>
+            <div className={styles.greeting__text_container}>
+              <h1 className={styles.greeting__text}>
+                Hello, My name is Jesuseyitan{" "}
+                <span className={styles.wave__emoji}>{emoji("👋🏾")}</span>
               </h1>
               <p className={styles.greeting__text_p}>
-                I am a passionate Full Stack Software Developer 🚀 having an
-                experience of building Web pplications with Reactjs / Next JS /
-                Nodejs / ASP.NET Core and some other cool libraries and
-                frameworks.
+                I am a passionate Full Stack Software Developer 🚀 building Web
+                Applications with Reactjs / Next JS / Nodejs / ASP.NET Core and
+                some other cool libraries and frameworks.
               </p>
             </div>
-
-            <div className="greeting__image_container">
+            {/* TODO - Add call to action Button: e.g Contact Me */}
+            <div className={styles.greeting__image_container}>
               <DisplayLottie animationData={welcomePerson} />
             </div>
           </div>
