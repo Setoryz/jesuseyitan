28797788cebb4e283da2346d69ffb1284d561512
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-12 17:38:48 +0100

    portfolio: PortfolioItem details modified - Image sub section added with overlay elements and link to see live - Details sub section added with description - useLayoutEffect added to change details subsection max-width to image width

--- portfolio/src/components/Portfolio/PortfolioItem.tsx
+++ portfolio/src/components/Portfolio/PortfolioItem.tsx
@@ -1,24 +1,47 @@
+import { useLayoutEffect, useRef, useState } from "react";
 import styles from "./PortfolioItem.module.scss";
 
 const PortfolioItem = () => {
+  const [detailsWidth, setDetailsWidth] = useState<null | number>(null);
+  const imageRef = useRef(null as null | HTMLImageElement);
+
+  const ResizeDetails = () => {
+    console.log("layout changed");
+    if (imageRef.current) setDetailsWidth(imageRef.current.clientWidth);
+  };
+  useLayoutEffect(() => {
+    ResizeDetails();
+    window.addEventListener("resize", ResizeDetails);
+    return () => {
+      window.removeEventListener("resize", ResizeDetails);
+    };
+  }, []);
+
   return (
     <div className={styles.portfolio__item_container}>
       <div className={styles.portfolio__item}>
-        <div className={styles.portfolio__item_details}>
-          <img
-            src="/assets/images/menu-banner-01.png"
-            alt=""
-            className={styles.portfolio__item_image}
-          />
-          <p className={styles.portfolio__item_category}>Item Details</p>
-        </div>
-        <div className={styles.portfolio__item_title}>
+        <a href="" className={styles.portfolio__item_image}>
+          <div className={styles.portfolio__item_defaultwrap}>
+            <img ref={imageRef} src="/assets/images/watch-tv.png" alt="" />
+          </div>
+          <div className={styles.portfolio__item_overlay}>
+            <div className={styles.overlay__ctabutton}></div>
+            <h3>See Live</h3>
+          </div>
+        </a>
+
+        <div
+          className={styles.portfolio__item_details}
+          style={{ maxWidth: detailsWidth ?? "100%" }}
+        >
           <div className={styles.portfolio__item_seperator}></div>
-          <h3>
-            <a href="" className={styles.portfolio__item_title}>
-              Item Title
-            </a>
-          </h3>
+          <a href="">
+            <h3>Watch TV - User</h3>
+          </a>
+          <p className={styles.description}>
+            The customer facing point of Watch TV app Technologies Used, React,
+            Redux, AWS S3, Express,{" "}
+          </p>
         </div>
       </div>
     </div>
