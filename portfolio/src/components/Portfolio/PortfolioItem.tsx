fbb434dfb5a607ee20b83dd74c0c8aed47f83060
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-12 18:58:42 +0100

    portfolio: Portfolioitem updated - technologies used added to details sub section - all content extracted to portfolioItem object

--- portfolio/src/components/Portfolio/PortfolioItem.tsx
+++ portfolio/src/components/Portfolio/PortfolioItem.tsx
@@ -20,9 +20,17 @@ const PortfolioItem = () => {
   return (
     <div className={styles.portfolio__item_container}>
       <div className={styles.portfolio__item}>
-        <a href="" className={styles.portfolio__item_image}>
+        <a
+          href={portfolioItem.project_url}
+          target="_blank"
+          className={styles.portfolio__item_image}
+        >
           <div className={styles.portfolio__item_defaultwrap}>
-            <img ref={imageRef} src="/assets/images/watch-tv.png" alt="" />
+            <img
+              ref={imageRef}
+              src={"/assets/images/" + portfolioItem.image}
+              alt=""
+            />
           </div>
           <div className={styles.portfolio__item_overlay}>
             <div className={styles.overlay__ctabutton}></div>
@@ -34,13 +42,14 @@ const PortfolioItem = () => {
           className={styles.portfolio__item_details}
           style={{ maxWidth: detailsWidth ?? "100%" }}
         >
-          <div className={styles.portfolio__item_seperator}></div>
-          <a href="">
-            <h3>Watch TV - User</h3>
+          <a href={portfolioItem.project_url} target="_blank">
+            <h3>{portfolioItem.title}</h3>
           </a>
-          <p className={styles.description}>
-            The customer facing point of Watch TV app Technologies Used, React,
-            Redux, AWS S3, Express,{" "}
+          <p className={styles.description}>{portfolioItem.description}</p>
+          <p className={styles.technologies_used}>
+            {portfolioItem.technologies_used.map((item, i) => (
+              <span key={i}>{item}</span>
+            ))}
           </p>
         </div>
       </div>
@@ -49,3 +58,11 @@ const PortfolioItem = () => {
 };
 
 export default PortfolioItem;
+
+const portfolioItem = {
+  title: "Watch TV - User",
+  description: `The customer facing point of Watch TV app built using react. It authenticate users and allows them to browse through a catalog of movies. Users can add movies to watchlist and go to the movie page to see more details, watch the trailer or watch the movie`,
+  technologies_used: ["React", "Redux", "AWS S3", "Express"],
+  image: "watch-tv.png",
+  project_url: "https://watch-tv.jesuseyitan.com",
+};
