1a19fe3aa222219fc6719629357db6da103e9843
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-18 17:02:02 +0100

    portfolio: validateFormData onChange test added

--- portfolio/src/components/Contact/utils/validateFormData.test.ts
+++ portfolio/src/components/Contact/utils/validateFormData.test.ts
@@ -1,19 +1,23 @@
-import { validateEmail, validateEmptyField } from "./validateFormData";
+import { ContactFormDataKeys } from "./types";
+import validateFormData, {
+  validateEmail,
+  validateEmptyField,
+} from "./validateFormData";
 
 describe("Email validation check", () => {
-  it("Should fail on value that is not email", () => {
+  it("Should fail on value that is not email = 'seyitan'", () => {
     expect(validateEmail("seyitan")).toBeFalsy();
   });
-  it("Should fail on value that is not email", () => {
+  it("Should fail on value that is not email = 'seyitan@.com'", () => {
     expect(validateEmail("seyitan@.com")).toBeFalsy();
   });
-  it("Should fail on value that is not email", () => {
+  it("Should fail on value that is not email = 'seyitan.com'", () => {
     expect(validateEmail("seyitan.com")).toBeFalsy();
   });
-  it("Should pass when value is an email", () => {
+  it("Should pass when value is an email = 'seyitan@gmail.com'", () => {
     expect(validateEmail("seyitan@gmail.com")).toBeTruthy();
   });
-  it("Should pass when value is an email", () => {
+  it("Should pass when value is an email = 'mee@jesuseyitan.com.ng'", () => {
     expect(validateEmail("mee@jesuseyitan.com.ng")).toBeTruthy();
   });
 });
@@ -27,5 +31,25 @@ describe("Empty Field Validation Check", () => {
   });
   it("should return true when string is not empty", () => {
     expect(validateEmptyField("something")).toBeTruthy();
+  });
+});
+
+describe("Validate Form Data OnChange Check", () => {
+  it("Should return true when fields are empty", () => {
+    const sampleFormData = { name: "", email: "", subject: "", message: "" };
+    Object.entries(sampleFormData).forEach(([key, value]) => {
+      expect(validateFormData(key as ContactFormDataKeys, value)).toBeTruthy();
+    });
+  });
+  it("Should return false when fields are valid", () => {
+    const sampleFormData = {
+      name: "Odukoya",
+      email: "seyi@g.co",
+      subject: "Something",
+      message: "A Long Message",
+    };
+    Object.entries(sampleFormData).forEach(([key, value]) => {
+      expect(validateFormData(key as ContactFormDataKeys, value)).toBeFalsy();
+    });
   });
 });
