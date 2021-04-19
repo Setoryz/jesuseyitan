c5676a1bac05c34f5f1f4bb521de359835524f43
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-19 20:15:01 +0100

    ✨ [portfolio, ContactForm] - loaders created

---
+++ portfolio/src/components/Loaders/Done/Done.tsx
@@ -0,0 +1,6 @@
+import styles from "./Done.module.scss";
+const Done = () => {
+  return <div className={styles.done}></div>;
+};
+
+export default Done;
---
+++ portfolio/src/components/Loaders/InProgress/InProgress.tsx
@@ -0,0 +1,6 @@
+import styles from "./InProgress.module.scss";
+const InProgress = () => {
+  return <div className={styles.in_progress}></div>;
+};
+
+export default InProgress;
