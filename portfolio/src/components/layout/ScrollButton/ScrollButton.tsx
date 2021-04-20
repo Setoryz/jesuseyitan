3a512bf2d7da9b3bf903e45de52a69b8b8878b9c
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-20 02:31:51 +0100

    💄 [portfolio, ScrollButton] - animation added to scroll button

--- portfolio/src/components/layout/ScrollButton/ScrollButton.tsx
+++ portfolio/src/components/layout/ScrollButton/ScrollButton.tsx
@@ -2,10 +2,11 @@ import styles from "./ScrollButton.module.scss";
 
 const ScrollButton = () => {
   return (
-    <div>
-      <div className={styles.scrollbutton__container}>
-        <span className={styles.scrollbutton}></span>
-      </div>
+    <div className={styles.scrollbutton__container}>
+      {/* <span className={styles.scrollbutton}></span> */}
+      <span className={styles.scrollbutton__main}></span>
+      <span className={styles.scrollbutton__main_2}></span>
+      <span className={styles.scrollbutton__main_3}></span>
     </div>
   );
 };
