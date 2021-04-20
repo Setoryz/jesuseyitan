7bf1bdcbd77ba564441478d689d01d210306fc5c
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-20 01:40:24 +0100

    🎨 [porfolio, pages] - index sections converted to pages

---
+++ portfolio/src/pages/Portfolio.tsx
@@ -0,0 +1,11 @@
+import Portfolio from "../components/Portfolio/Portfolio";
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <Portfolio />
+    </div>
+  );
+};
+
+export default PortfolioPage;
