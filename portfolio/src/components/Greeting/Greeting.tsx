91430f2030709f73a0fdb4fd28a48b3704feb672
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-20 18:23:37 +0100

    ✨ 💄 [portfolio, Greeting] - Add cta button and improved styling

--- portfolio/src/components/Greeting/Greeting.tsx
+++ portfolio/src/components/Greeting/Greeting.tsx
@@ -4,6 +4,7 @@ import welcomePerson from "../../assets/lottie/welcomePerson.lottie.json";
 import DisplayLottie from "../../utils/DisplayLottie/DisplayLottie";
 import { fade_in_out, variantsPageSection } from "../../constants/variants";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Greeting = () => {
   return (
@@ -11,21 +12,29 @@ const Greeting = () => {
       <div className={styles.greeting}>
         <div className={styles.greet__main}>
           <div className={styles.greeting__main}>
-            <motion.div
-              variants={variantsPageSection}
-              className={styles.greeting__text_container}
-            >
-              <h1 className={styles.greeting__text}>
+            <div className={styles.greeting__text_container}>
+              <motion.h1
+                variants={variantsPageSection}
+                className={styles.greeting__text}
+              >
                 Hello, My name is <br /> Jesuseyitan{" "}
                 <span className={styles.wave__emoji}>{emoji("👋🏾")}</span>
-              </h1>
-              <p className={styles.greeting__text_p}>
-                I am a passionate Full Stack Software Developer 🚀 building Web
-                Applications with Reactjs / Next JS / Nodejs / ASP.NET Core and
-                some other cool libraries and frameworks.
-              </p>
-            </motion.div>
-            {/* TODO - Add call to action Button: e.g Contact Me */}
+              </motion.h1>
+              <motion.div
+                className={styles.greeting__text_details}
+                variants={variantsPageSection}
+              >
+                <p className={styles.greeting__text_p}>
+                  I am a passionate Full Stack Software Developer 🚀 building
+                  Web Applications with Reactjs / Next JS / Nodejs / ASP.NET
+                  Core and some other cool libraries and frameworks.
+                </p>
+
+                <Link href="/Contact">
+                  <button>Contact Me</button>
+                </Link>
+              </motion.div>
+            </div>
             <motion.div
               variants={variantsPageSection}
               className={styles.greeting__image_container}
