a1767f06a60a5b43fd807650a4c8a6c55bda8cb6
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-20 17:04:15 +0100

    💫 [portfolio, WhatIdo Page] - animation added to page and sections

--- portfolio/src/components/Skills/Skills.tsx
+++ portfolio/src/components/Skills/Skills.tsx
@@ -2,24 +2,43 @@ import DisplayLottie from "../../utils/DisplayLottie/DisplayLottie";
 import styles from "./Skills.module.scss";
 import codingPerson from "../../assets/lottie/codingPerson.lottie.json";
 import SkillsIcons from "./SkillsIcons";
+import { motion } from "framer-motion";
+import { variantsPageSection } from "../../constants/variants";
 
 const Skills = () => {
   return (
     <div className={styles.skills}>
       <div className={styles.skills__main}>
-        <div className={styles.skills__image}>
+        <motion.div
+          variants={variantsPageSection}
+          className={styles.skills__image}
+        >
           <DisplayLottie animationData={codingPerson} />
-        </div>
+        </motion.div>
 
         <div className={styles.skills__text}>
-          <h1 className={styles.skills__text_heading}>What I do</h1>
+          <motion.h1
+            variants={variantsPageSection}
+            className={styles.skills__text_heading}
+          >
+            What I do
+          </motion.h1>
 
-          <p className={styles.skills__text_subtitle}>
+          <motion.p
+            variants={variantsPageSection}
+            className={styles.skills__text_subtitle}
+          >
             FULL STACK DEVELOPER BUILDING HIGHLY SCALABLE APPLICATIONS
-          </p>
+          </motion.p>
 
-          <SkillsIcons />
-          <div className={styles.skills__text_list}>
+          <motion.div variants={variantsPageSection}>
+            <SkillsIcons />
+          </motion.div>
+
+          <motion.div
+            variants={variantsPageSection}
+            className={styles.skills__text_list}
+          >
             {SkillsTextList.map((skill, index) => (
               <p
                 key={index + skill[0]}
@@ -28,7 +47,7 @@ const Skills = () => {
                 ⚡ {skill}
               </p>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
--- portfolio/src/pages/WhatIDo.tsx
+++ portfolio/src/pages/WhatIDo.tsx
@@ -2,10 +2,16 @@ import { motion } from "framer-motion";
 import Head from "next/head";
 import RouteNav from "../components/layout/RouteNav/RouteNav";
 import Skills from "../components/Skills/Skills";
+import { fade_in_out, variantsPage } from "../constants/variants";
 
 const SkillsPage = () => {
   return (
-    <motion.div exit={{ opacity: 0 }} animate={{ opacity: 1 }}>
+    <motion.div
+      initial="initial"
+      animate="animate"
+      exit="initial"
+      variants={variantsPage}
+    >
       <Head>
         <title>What I do - Jesuseyitan</title>
       </Head>
