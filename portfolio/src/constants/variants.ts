88f8fd80f102e54f2204437b2ed0d189b5a72954
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-22 10:03:44 +0100

    ✨💫 [portfolio, motionVariants, fixes #13] - Motion Variants created for SitePreloader Logo animation

--- portfolio/src/constants/variants.ts
+++ portfolio/src/constants/variants.ts
@@ -18,13 +18,85 @@ export const variantsPreloaderContainer: Variants = {
 export const variantesPreloaderSplashBox: Variants = {
   initial: {
     height: "100vh",
-    // bottom: 0,
   },
   animate: {
     height: 0,
     transition: {
-      //   when: "afterChildren",
       duration: 1.5,
+      ease: [0.87, 0, 0.13, 1],
+      delay: 3.5,
+    },
+  },
+};
+export const variantsLogoMainFadeIn: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.3,
+      duration: 0.7,
+      ease: [0.87, 0, 0.13, 1],
+    },
+  },
+};
+
+export const variantsLogoMainSlideOut: Variants = {
+  initial: {
+    opacity: 1,
+    x: "9rem",
+  },
+  animate: {
+    opacity: 0,
+    x: "2.5rem",
+    transition: {
+      when: "afterChildren",
+      delay: 0.5,
+      duration: 1.0,
+      ease: [0.87, 0, 0.13, 1],
+    },
+  },
+};
+
+export const variantsLogoSecondarySlideIn: Variants = {
+  initial: {
+    x: "-11.5rem",
+  },
+  animate: {
+    x: "-2.5rem",
+    transition: { duration: 1.0, delay: 1.5 },
+  },
+};
+
+export const variantsLogoSecondary: Variants = {
+  initial: {
+    opacity: 1,
+    y: 0,
+  },
+  animate: {
+    opacity: 0,
+    y: "calc(-50vh + 4rem)",
+    transition: {
+      delay: 0.5,
+      delayChildren: 1.55,
+      staggerChildren: 0.04,
+      staggerDirection: -1,
+      duration: 1.5,
+      when: "afterChildren",
+      ease: [0.87, 0, 0.13, 1],
+    },
+  },
+};
+
+export const variantsLogoSecondaryPath: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
       ease: [0.87, 0, 0.13, 1],
     },
   },
