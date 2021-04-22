08c0d9e882df35209677a4be3fec409df3748b3f
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-22 02:16:58 +0100

    🚀 🐛 [portfolio, page transitions] - Fix #10 : - Keep module styles after route change

--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -5,8 +5,35 @@ import Header from "../components/layout/Header/Header";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
 import SitePreloader from "../components/Loaders/SitePreloader/SitePreloader";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
+  // ** Fix for Styles immediately route change is started
+  useEffect(() => {
+    Array.from(
+      document.querySelectorAll('head > link[rel="stylesheet"][data-n-p]')
+    ).forEach((node) => {
+      node.removeAttribute("data-n-p");
+    });
+    const mutationHandler = (mutations: any) => {
+      mutations.forEach(({ target }: any) => {
+        if (target.nodeName === "STYLE") {
+          if (target.getAttribute("media") === "x") {
+            target.removeAttribute("media");
+          }
+        }
+      });
+    };
+    const observer = new MutationObserver(mutationHandler);
+    observer.observe(document.head, {
+      subtree: true,
+      attributeFilter: ["media"],
+    });
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <Head>
