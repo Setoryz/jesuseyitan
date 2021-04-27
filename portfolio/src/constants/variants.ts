791228f13d14fa36cc57a21e624d1dd9dcd486d5
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-04-27 08:08:52 +0100

    💫 [motionVariants] - fadeIn variants added for PortfolioItemDetails background

--- portfolio/src/constants/variants.ts
+++ portfolio/src/constants/variants.ts
@@ -128,3 +128,13 @@ export const variantsPageSection: Variants = {
     },
   },
 };
+export const variantsDetailsFadeIn: Variants = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.6, -0.05, 0.01, 0.99],
+    },
+  },
+};
