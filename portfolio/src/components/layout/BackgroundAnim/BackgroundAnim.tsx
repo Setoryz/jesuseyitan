e5de82890532c6b064fbc3c5d3ee1ceba87fb397
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-31 20:30:29 +0100

    💫 [Portfolio, backgroundAnim] - increased backgroundAnim particles

--- portfolio/src/components/layout/BackgroundAnim/BackgroundAnim.tsx
+++ portfolio/src/components/layout/BackgroundAnim/BackgroundAnim.tsx
@@ -3,17 +3,7 @@ import styles from "./BackgroundAnim.module.scss";
 const BackgroundAnim = () => {
   return (
     <div className={styles.backgroundAnim}>
-      {/* <svg version='1.1' xmlns='http://www.w3.org/2000/svg'>
-  <defs>
-    <filter id='goo'>
-      <fegaussianblur in='SourceGraphic' result='blur' stddeviation='10'></fegaussianblur>
-      <fecolormatrix in='blur' mode='matrix' result='goo' values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7'></fecolormatrix>
-      <feblend in2='goo' in='SourceGraphic'></feblend>
-    </filter>
-  </defs>
-</svg> */}
-
-      {[...Array(200)].map((element, index) => (
+      {[...Array(400)].map((element, index) => (
         <span key={index} className={styles.particle}></span>
       ))}
     </div>
